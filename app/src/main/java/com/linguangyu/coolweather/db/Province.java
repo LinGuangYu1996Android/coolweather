ebac04026c94a058b04f61035915f64d21391230
LinGuangYu <13430348380@163.com>
2017-05-21 00:13:33 +0800

    First commit.

---
+++ app/src/main/java/com/linguangyu/coolweather/db/Province.java
@@ -0,0 +1,41 @@
+package com.linguangyu.coolweather.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by 光裕 on 2017/5/20.
+ */
+
+public class Province extends DataSupport {
+
+    private int id;
+
+    private String provinceName;
+
+    private int provinceCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public int getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public void setProvinceCode(int provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+
+}
